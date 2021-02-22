ff3286a4915a8700bba455bb0cc90bcd30b1d8b7
Ben Fulton <benjamin.fulton99@gmail.com>
2021-02-22 09:49:40 -0700

    Create RideMechanics join table]

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+mech_1 = Mechanic.create(name: 'A', experience: 12)
+mech_2 = Mechanic.create(name: 'B', experience: 13)
+mech_3 = Mechanic.create(name: 'C', experience: 14)
+mech_4 = Mechanic.create(name: 'D', experience: 15)
+
+ride_1 = Ride.create(name: 'Mind Eraser', thrills: 211)
+ride_2 = Ride.create(name: 'Space Mountain', thrills: 413)
+ride_3 = Ride.create(name: 'Scrambler', thrills: 115)
+ride_4 = Ride.create(name: 'Twister', thrills: 610)
+
+mech_1.rides << ride_1
+mech_2.rides << ride_2
+mech_3.rides << ride_3
+mech_3.rides << ride_4
