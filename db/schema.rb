d92c5a4997db7ea1fa11161b0d6bc07a0dc5822e
Ben Fulton <benjamin.fulton99@gmail.com>
2021-02-22 10:18:31 -0700

    Add default value to ride is_open

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_163139) do
+ActiveRecord::Schema.define(version: 2021_02_22_165623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_02_22_163139) do
   create_table "rides", force: :cascade do |t|
     t.string "name"
     t.integer "thrills"
+    t.boolean "is_open", default: false
   end
 
   add_foreign_key "ride_mechanics", "mechanics"
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,10 @@ mech_2 = Mechanic.create(name: 'B', experience: 13)
 mech_3 = Mechanic.create(name: 'C', experience: 14)
 mech_4 = Mechanic.create(name: 'D', experience: 15)
 
-ride_1 = Ride.create(name: 'Mind Eraser', thrills: 211)
-ride_2 = Ride.create(name: 'Space Mountain', thrills: 413)
-ride_3 = Ride.create(name: 'Scrambler', thrills: 115)
-ride_4 = Ride.create(name: 'Twister', thrills: 610)
+ride_1 = Ride.create(name: 'Mind Eraser', thrills: 211, is_open: true)
+ride_2 = Ride.create(name: 'Space Mountain', thrills: 413, is_open: true)
+ride_3 = Ride.create(name: 'Scrambler', thrills: 115, is_open: true)
+ride_4 = Ride.create(name: 'Twister', thrills: 610, is_open: true)
 
 mech_1.rides << ride_1
 mech_2.rides << ride_2
