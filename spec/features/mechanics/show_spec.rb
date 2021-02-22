5f5081df3340bf88292a68d91d4266cfad72f365
Ben Fulton <benjamin.fulton99@gmail.com>
2021-02-22 11:19:29 -0700

    Write tests for adding rides to mechanics

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -1,23 +1,23 @@
 require "rails_helper"
 RSpec.describe 'As a user'do
-before :each do
-	Mechanic.destroy_all
-	Ride.destroy_all
-	@mech_1 = Mechanic.create(name: 'A', experience: 12)
-	@mech_2 = Mechanic.create(name: 'B', experience: 13)
-	@mech_3 = Mechanic.create(name: 'C', experience: 14)
-	@mech_4 = Mechanic.create(name: 'D', experience: 15)
+	before :each do
+		Mechanic.destroy_all
+		Ride.destroy_all
+		@mech_1 = Mechanic.create(name: 'A', experience: 12)
+		@mech_2 = Mechanic.create(name: 'B', experience: 13)
+		@mech_3 = Mechanic.create(name: 'C', experience: 14)
+		@mech_4 = Mechanic.create(name: 'D', experience: 15)
 
-	@ride_1 = Ride.create(name: 'Mind Eraser', thrills: 211, is_open: true)
-	@ride_2 = Ride.create(name: 'Space Mountain', thrills: 413, is_open: true)
-	@ride_3 = Ride.create(name: 'Scrambler', thrills: 115, is_open: true)
-	@ride_4 = Ride.create(name: 'Twister', thrills: 610, is_open: true)
+		@ride_1 = Ride.create(name: 'Mind Eraser', thrills: 211, is_open: true)
+		@ride_2 = Ride.create(name: 'Space Mountain', thrills: 413, is_open: true)
+		@ride_3 = Ride.create(name: 'Scrambler', thrills: 115, is_open: true)
+		@ride_4 = Ride.create(name: 'Twister', thrills: 610, is_open: true)
 
-	@mech_1.rides << @ride_1
-	@mech_2.rides << @ride_2
-	@mech_3.rides << @ride_3
-	@mech_3.rides << @ride_4
-end
+		@mech_1.rides << @ride_1
+		@mech_2.rides << @ride_2
+		@mech_3.rides << @ride_3
+		@mech_3.rides << @ride_4
+	end
 	describe'when I visit the show page'do
 		it'lists their name, experience, and all the rides theyre working on'do
 			visit "mechanics/#{@mech_1.id}"
@@ -36,6 +36,13 @@ end
 
 			within("#open-rides")do
 				expect(@ride_4.name).to appear_before(@ride_3.name)
+			end
+			fill_in "ride_id" , with: "#{@ride_1.id}"
+			click_on "Submit"
+
+			within("#open-rides")do
+				expect(@ride_4.name).to appear_before(@ride_1.name)
+				expect(@ride_1.name).to appear_before(@ride_3.name)
 			end
 		end
 
