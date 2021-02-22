f518534acfb1f6c1c5440b73fc563a8e502ef8b8
Ben Fulton <benjamin.fulton99@gmail.com>
2021-02-22 10:18:58 -0700

    Write tests for mechanics show page

---
+++ spec/features/mechanics/show_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+RSpec.describe 'As a user'do
+before :each do
+	Mechanic.destroy_all
+	Ride.destroy_all
+	@mech_1 = Mechanic.create(name: 'A', experience: 12)
+	@mech_2 = Mechanic.create(name: 'B', experience: 13)
+	@mech_3 = Mechanic.create(name: 'C', experience: 14)
+	@mech_4 = Mechanic.create(name: 'D', experience: 15)
+
+	@ride_1 = Ride.create(name: 'Mind Eraser', thrills: 211, is_open: true)
+	@ride_2 = Ride.create(name: 'Space Mountain', thrills: 413, is_open: true)
+	@ride_3 = Ride.create(name: 'Scrambler', thrills: 115, is_open: true)
+	@ride_4 = Ride.create(name: 'Twister', thrills: 610, is_open: true)
+
+	@mech_1.rides << @ride_1
+	@mech_2.rides << @ride_2
+	@mech_3.rides << @ride_3
+	@mech_3.rides << @ride_4
+end
+	describe'when I visit the show page'do
+		it'lists their name, experience, and all the rides theyre working on'do
+			visit "mechanics/#{@mech_1.id}"
+			expect(page).to have_content("Mechanic: #{@mech_1.name}")
+			expect(page).to have_content("Years of Experience: #{@mech_1.experience}")
+
+			within("#open-rides")do
+				expect(page).to have_content("#{@ride_1.name}")
+			end
+			
+		end
+
+	end
+end
