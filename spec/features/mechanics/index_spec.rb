97d975c6df903e018b58e467524cc5c6de13dfbf
Ben Fulton <benjamin.fulton99@gmail.com>
2021-02-22 09:36:37 -0700

    Write relationship tests for all models

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -3,6 +3,7 @@ RSpec.describe 'As a user'do
 	describe'when I visit the index page'do
 		it'lists all mechanics and their years'do
 			visit'/mechanics'
+			expect(page).to have_content("All Mechanics")
 		end
 	end
 end
